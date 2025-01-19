3bd837c25be7a208b8a232e581b4b6069fbb0f63
averiefiedler <avefied@gmail.com>
2025-01-19 08:21:45 -0500

    Add files via upload
    puzzle minigame scripts (for now) (for safekeeping) (i fear im writing a new one) (kill me)

---
+++ PuzzleSlot.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSlot : MonoBehaviour
+{
+    public SpriteRenderer Renderer; 
+
+    public void Placed()
+    {
+      
+    }
+}
